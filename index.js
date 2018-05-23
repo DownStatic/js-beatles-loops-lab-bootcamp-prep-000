9e708a1f43384e7b909ddcf467baa3bdaab93bc9
Christopher Stull <chris.stull@gmail.com>
2018-05-23 22:37:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians,instruments){
+  var plays = []
+  for(i = 0; i < musicians.length; i++){
+    plays[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return plays
+}
+
